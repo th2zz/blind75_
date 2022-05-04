6082b33b6c406ff077673619aa253c6c81b7c20b
th2zz <tzhang329@gmail.com>
2022-05-04 20:50:16 +0800

    add files

---
+++ dp/decode_ways.py
@@ -0,0 +1,4 @@
+class Solution:  # https://leetcode-cn.com/problems/decode-ways/
+    # give a digit str, return #ways to decode it to ascii letter str. A=1,B=2...
+    def numDecodings(self, s: str) -> int:
+        pass
