2d365c5af60b75008fd5e26f7d0a838f4626460b
th2zz <tzhang329@gmail.com>
2022-05-13 15:17:38 +0800

    add files

--- dp/coordinate_dp/house_robber.py
+++ dp/coordinate_dp/house_robber.py
@@ -24,4 +24,13 @@ dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
 
 class Solution:  # https://leetcode.cn/problems/house-robber/
     def rob(self, nums: List[int]) -> int:
-        pass
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        dp = [0] * len(nums)  # dp[i] amount of money we can rob at house i, so far
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        return dp[len(nums) - 1]
